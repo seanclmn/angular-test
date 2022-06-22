01b6bee53e1edc462356139ceecc8b086c46fe8f
claygrumeo <48027365+claygrumeo@users.noreply.github.com>
2022-06-22 15:02:18 -0400

    local papi call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,25 @@ interface Fact {
 export class AppComponent {
   constructor(private http: HttpClient) {}
   title = 'ang1';
-  fact = '';
-
+  providers:string[] = [];
+  
   makecall(): void {
-    this.http.get('/fact').subscribe((data: any) => {
+    const headers = {
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTU5MjY0MDcsImlhdCI6MTY1NTkyMjgwNywiaXNzIjoibG1hIiwianRpIjoiTVh2MGRGd1M5dSIsInN1YiI6ImNsYXlAcGVyaW9kaWMuaXMifQ.wGoiTCPS5jQVaHvtoIknrjgx3KPmc-XFYoaCcAXLQcA',
+      'Content-Type': 'application/json'
+    }
+    const body = {
+      "jsonrpc": "2.0",
+      "method": "get_providers",
+      "params":{},
+      "id": "12345"
+    }
+    this.http.post('/', body, { headers }).subscribe((data: any) => {
       console.log("DATA", data)
-      this.fact = data.fact
+      this.providers = [];
+      data.result.forEach((element: any) => {
+        this.providers.push(JSON.stringify(element.name))
+      });
     })
   }
 }
